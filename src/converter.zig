5837dc3ef5e6e77d8f1ae7a7c98b6e91a669905b
Casper Eijkens <caspereijkens@icloud.com>
2026-02-04 18:39:06 +0100

    Format error message a bit nicer.

--- src/converter.zig
+++ src/converter.zig
@@ -854,7 +854,7 @@ pub const Converter = struct {
                 if (!std.mem.eql(u8, voltage_level1_id, voltage_level2_id)) {
                     const voltage_level1 = self.getVoltageLevel(network, voltage_level1_id) orelse return error.MalformedXML;
                     const voltage_level2 = self.getVoltageLevel(network, voltage_level2_id) orelse return error.MalformedXML;
-                    print.stderr("conversion failed for {s} {s} because of a voltage level mismatch: {s} != {s}", .{ mapping.cim_type, name.?, voltage_level1.name.?, voltage_level2.name.? });
+                    print.stderr("conversion failed for {s} '{s}' because of a voltage level mismatch: '{s}' != '{s}'", .{ mapping.cim_type, name.?, voltage_level1.name.?, voltage_level2.name.? });
                     return error.MalformedXML;
                 }
 
