9c4d7665fdd2b0a2b8a63446340f6dc468c7bc95
Casper Eijkens <caspereijkens@icloud.com>
2026-02-07 09:36:20 +0100

    Add line break.

--- src/converter.zig
+++ src/converter.zig
@@ -854,7 +854,7 @@ pub const Converter = struct {
                 if (!std.mem.eql(u8, voltage_level1_id, voltage_level2_id)) {
                     const voltage_level1 = self.getVoltageLevel(network, voltage_level1_id) orelse return error.MalformedXML;
                     const voltage_level2 = self.getVoltageLevel(network, voltage_level2_id) orelse return error.MalformedXML;
-                    try print.stdout("Error: conversion failed for {s} '{s}' because of a voltage level mismatch: '{s}' != '{s}'", .{ mapping.cim_type, name.?, voltage_level1.name.?, voltage_level2.name.? });
+                    try print.stdout("Error: conversion failed for {s} '{s}' because of a voltage level mismatch: '{s}' != '{s}'\n", .{ mapping.cim_type, name.?, voltage_level1.name.?, voltage_level2.name.? });
                     continue; // TODO revert back to strict check.
                     // TODO: return error.MalformedXML;
                 }
